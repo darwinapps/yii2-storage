7f73a6b1d4e0c04d384c656190c95aa87e9806b3
Yury Bratukhin <page2up@gmail.com>
2015-10-22 12:49:28 +0300

    Hybrid Google Drive - add MetaData file

---
+++ adapters/hybrid/MetaData.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace darwinapps\storage\adapters\hybrid;
+
+class MetaData extends \yii\base\Model
+{
+    public $id;
+
+    public $preview_name;
+    public $preview_type;
+    public $preview_time;
+
+    public $origin_name;
+    public $origin_type;
+    public $origin_time;
+}
